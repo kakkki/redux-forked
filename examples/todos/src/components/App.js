6674a1ee3ef2a35598b4106ef6fb0b051ab4e717
kakehi-binc <kakehi@binc.jp>
2020-02-22 15:21:51 +0900

    addTodo Dummy

--- examples/todos/src/components/App.js
+++ examples/todos/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
+import AddTodoDummy from '../containers/AddTodoDummy'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
     <AddTodo />
+    <AddTodoDummy />
     <VisibleTodoList />
     <Footer />
   </div>
